fdb5a20972b50506e1fa5698df1a3764f8672db5
ariel.serato <ariel.serato@megatrans.com.ar>
2022-08-26 13:42:02 -0300

    add files to project

---
+++ ASPWorker/Program.cs
@@ -0,0 +1,27 @@
+using ASPWorker;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHostedService<Worker>().AddSingleton<IFileData,FileData>();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
